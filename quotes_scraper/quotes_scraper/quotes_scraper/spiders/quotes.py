31c487420af3a43e30bcad227696869092a3a55d
Harvey Unsaac <193837@unsaac.edu.pe>
2022-08-14 23:36:44 +0200

    The method follow of response is implemented on spider

--- quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -1,10 +1,13 @@
 # Import de scrapy framework
+from ast import parse
+from gc import callbacks
 import scrapy
 
 # XPATH EXPRESSIONS
 # Titulo = //h1/a/text()
 # Citas = //span[@class="text" and @itemprop="text"]/text()
 # Top ten tag = //div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()
+# Boton next = //ul[@class="pager"]//li[@class="next"]/a/@href
 
 # Spider Class definition
 
@@ -20,31 +23,46 @@ class QuotesSpider(scrapy.Spider):
         'http://quotes.toscrape.com/page/1'
     ]
 
+    # Settings output
+
+    # custom_settings = {
+    #     'FEED_URI': 'quotes.json',
+    #     'FEED_FORMAT': 'json'
+    # }
+
+    custom_settings = {
+        'FEEDS': {
+            'quotes.json' : {
+                'format': 'json',
+                'encoding': 'utf-8',
+                'store_empty': False,
+                'fields': None,
+                'indent': 4,
+                'item_export_kwargs': {
+                    'export_empty_fields': True
+                }
+
+            }
+        }
+    }
+
     # Define obligatory method into Spider Class
     # This method analizes a file to extract information
     # and extrat information we need
     def parse(self, response):
-        print('*'*20)
-        print('\n')
-
+        
         title = response.xpath('//h1/a/text()').get()
-        print(f'Titulo: {title}')
-        print('\n')
-
+ 
         quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
-        print(f'Citas: ')
-        for quote in quotes:
-            print(f'- {quote} ')
-        print('\n')
 
         top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()').getall()
-        i = 0
-        for tag in top_ten_tags:
-            i += 1
-            print(f'{i}.- {tag}')
-        print('\n')
-        
-        #print(response.status, response.headers)
-        print('*'*20)
-        print('\n\n')
 
+        yield {
+            'title': title,
+            'quotes': quotes,
+            'top_ten_tags': top_ten_tags
+        }
+
+        nex_page_button_link = response.xpath('//ul[@class="pager"]//li[@class="next"]/a/@href').get()
+        if nex_page_button_link:
+            yield response.follow(nex_page_button_link, callback = self.parse)
