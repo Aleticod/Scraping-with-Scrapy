896628ac141b16bbaefb7d025c552c4c89195087
Harvey Unsaac <193837@unsaac.edu.pe>
2022-08-14 22:27:52 +0200

    Spider quotes extract the title, quotes an top ten tags

--- quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
+++ quotes_scraper/quotes_scraper/quotes_scraper/spiders/quotes.py
@@ -1,6 +1,10 @@
 # Import de scrapy framework
 import scrapy
 
+# XPATH EXPRESSIONS
+# Titulo = //h1/a/text()
+# Citas = //span[@class="text" and @itemprop="text"]/text()
+# Top ten tag = //div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()
 
 # Spider Class definition
 
@@ -22,7 +26,25 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         print('*'*20)
         print('\n')
-        print(response.status, response.headers)
+
+        title = response.xpath('//h1/a/text()').get()
+        print(f'Titulo: {title}')
+        print('\n')
+
+        quotes = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
+        print(f'Citas: ')
+        for quote in quotes:
+            print(f'- {quote} ')
+        print('\n')
+
+        top_ten_tags = response.xpath('//div[contains(@class, "tags-box")]/span[@class="tag-item"]/a/text()').getall()
+        i = 0
+        for tag in top_ten_tags:
+            i += 1
+            print(f'{i}.- {tag}')
+        print('\n')
+        
+        #print(response.status, response.headers)
         print('*'*20)
         print('\n\n')
 
